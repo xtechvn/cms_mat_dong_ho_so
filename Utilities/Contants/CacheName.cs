19eaf953d163424697126902d2f98b64a04d45ed
IamBemooooo <nguyendinhthe405@gmail.com>
2024-12-04 14:08:53 +0700

    change cache;s name

--- Utilities/Contants/CacheName.cs
+++ Utilities/Contants/CacheName.cs
@@ -6,30 +6,30 @@ namespace Utilities.Contants
 {
     public struct CacheName
     {
-        public const string PROVIDER_LIST = "PROVIDER_LIST_OCLCOK_";
-        public const string HOTEL_ROOM = "HOTEL_ROOM_OCLCOK_";
-        public const string ROOM_PACKAGES = "ROOM_PACKAGE_OCLCOK_";
-        public const string ROOM_FUN = "ROOM_FUN_OCLCOK_";
-        public const string PROVINCE = "PROVINCE_OCLCOK_";
-        public const string SUPPLIER = "SUPPLIER_OCLCOK_";
-        public const string CACHE_TELEGRAM_LIST = "CACHE_TELEGRAM_LIST_OCLCOK_";
-        public const string NOTIFY = "NOTIFY_OCLCOK_";
-        public const string PLAYGROUND_DETAIL = "PLAYGROUND_DETAIL_OCLCOK_";
-        public const string B2C_TOUR_SEARCH = "B2C_TOUR_SEARCH_OCLCOK_";
-        public const string LIST_TOUR_TYPE = "LIST_TOUR_TYPE_OCLCOK_";
-        public const string ARTICLE_B2C_CATEGORY_MENU = "ARTICLE_CATEGORY_MENU_OCLCOK_";
-        public const string USER_ROLE = "USER_ROLE_OCLCOK_";
-        public const string ARTICLE = "ARTICLE_OCLCOK_";
+        public const string PROVIDER_LIST = "PROVIDER_LIST_OCLOCK_";
+        public const string HOTEL_ROOM = "HOTEL_ROOM_OCLOCK_";
+        public const string ROOM_PACKAGES = "ROOM_PACKAGE_OCLOCK_";
+        public const string ROOM_FUN = "ROOM_FUN_OCLOCK_";
+        public const string PROVINCE = "PROVINCE_OCLOCK_";
+        public const string SUPPLIER = "SUPPLIER_OCLOCK_";
+        public const string CACHE_TELEGRAM_LIST = "CACHE_TELEGRAM_LIST_OCLOCK_";
+        public const string NOTIFY = "NOTIFY_OCLOCK_";
+        public const string PLAYGROUND_DETAIL = "PLAYGROUND_DETAIL_OCLOCK_";
+        public const string B2C_TOUR_SEARCH = "B2C_TOUR_SEARCH_OCLOCK_";
+        public const string LIST_TOUR_TYPE = "LIST_TOUR_TYPE_OCLOCK_";
+        public const string ARTICLE_B2C_CATEGORY_MENU = "ARTICLE_CATEGORY_MENU_OCLOCK_";
+        public const string USER_ROLE = "USER_ROLE_OCLOCK_";
+        public const string ARTICLE = "ARTICLE_OCLOCK_";
 
-        public const string PRODUCT_LISTING = "PRODUCT_LISTING_OCLCOK_";
-        public const string PRODUCT_DETAIL = "PRODUCT_DETAIL_OCLCOK_";
-        public const string ORDER_DETAIL_FE = "ORDER_DETAIL_FE_OCLCOK_";
-        public const string DISTRICT = "DISTRICT_OCLCOK_";
-        public const string WARD = "WARD_OCLCOK_";
+        public const string PRODUCT_LISTING = "PRODUCT_LISTING_OCLOCK_";
+        public const string PRODUCT_DETAIL = "PRODUCT_DETAIL_OCLOCK_";
+        public const string ORDER_DETAIL_FE = "ORDER_DETAIL_FE_OCLOCK_";
+        public const string DISTRICT = "DISTRICT_OCLOCK_";
+        public const string WARD = "WARD_OCLOCK_";
 
-        public const string PRODUCT_BRAND = "PRODUCT_BRAND_OCLCOK_";
-        public const string PRODUCT_BY_BRAND = "PRODUCT_BY_BRAND_OCLCOK_";
-        public const string PRODUCT_BY_PRICERANGE = "PRODUCT_BY_PRICERANGE_OCLCOK_";
+        public const string PRODUCT_BRAND = "PRODUCT_BRAND_OCLOCK_";
+        public const string PRODUCT_BY_BRAND = "PRODUCT_BY_BRAND_OCLOCK_";
+        public const string PRODUCT_BY_PRICERANGE = "PRODUCT_BY_PRICERANGE_OCLOCK_";
 
     }
 }
